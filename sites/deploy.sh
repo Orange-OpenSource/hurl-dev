36fc440103f66761636448c2d5c07b3df041e660
jcamiel <jeanchristophe.amiel@orange.com>
2021-01-21 13:12:53 +0100

    Erase extar files on dest when deploying.

--- sites/deploy.sh
+++ sites/deploy.sh
@@ -3,5 +3,5 @@ set -e
 set -u
 
 SITES_DIR=$(dirname "$0")
-rsync -avz -e "ssh -i $PRIVATE_KEY_FILE -o "StrictHostKeyChecking=no" -p $SSH_PORT" "$SITES_DIR/hurl.dev/_site/" "$SSH_USER"@hurl.dev:www
+rsync --archive --verbose --compress --delete -e "ssh -i $PRIVATE_KEY_FILE -o 'StrictHostKeyChecking=no' -p $SSH_PORT" "$SITES_DIR/hurl.dev/_site/" "$SSH_USER"@hurl.dev:www
 
